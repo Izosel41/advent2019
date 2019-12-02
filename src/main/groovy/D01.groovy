dfed2eb102e1d89b261d976aaba5b0b3af5db57f
Izosel41 <31371831+Izosel41@users.noreply.github.com>
2019-12-02 17:49:14 +0100

    day 01.2

--- src/main/groovy/D01.groovy
+++ src/main/groovy/D01.groovy
@@ -1,20 +1,23 @@
 class D01 {
 
     int fuel(List<String> masses) {
-		masses.inject(0){fuel, it -> it.toInteger().intdiv(3) -2 + fuel}
+		masses.inject(0){fuel, it -> fuel + calculateFuel(it) }
     }
 
-    def extrafuel(List<String> fuels) {
+    private int calculateFuel(String it) {
+        it.toInteger().intdiv(3) - 2
+    }
+
+    def extrafuel(List<String> masses) {
         Set memory = []
         int result = 0
         boolean found = false
 
         while (true) {
-            for (int fuel in fuels*.toInteger()) {
-                result = result + fuel
-                found = !memory.add(result)
-                if (found)
-                    return result
+            for (int mass in masses*.toInteger()) {
+                def fuelForMass = calculateFuel(mass)
+                def fuelForFuel = { long n -> n<0?n:calculateFuel(fuelForMass)}.memoize()
+                result = result + fuelForFuel + fuelForMass
             }
         }
     }
