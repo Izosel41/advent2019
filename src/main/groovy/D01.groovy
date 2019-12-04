75b4c684372804e20b12217a1a92b36f58f8624d
Izosel41 <31371831+Izosel41@users.noreply.github.com>
2019-12-04 01:19:41 +0100

    day01.2

--- src/main/groovy/D01.groovy
+++ src/main/groovy/D01.groovy
@@ -1,28 +1,34 @@
 class D01 {
 
     int fuel(List<String> masses) {
-        masses*.toInteger().inject(0, calculateFuel(0))
+        masses*.toInteger().inject(0, calculateFuel)
     }
 
-    def calculateFuel(int total, int mass) {
+    Closure calculateFuel = { total, mass ->
         int division = mass.intdiv(3)
-        division < 2 ? 0 : division - 2
+        division < 2 ? total : total + division - 2
     }
 
     def extraFuel(List<String> masses) {
-        masses.inject(0) { fuel, it ->
-            def fuelForMass = calculateFuel(it.toInteger())
-
-            def fuelForFuel = {fm, def accu = 0 ->
-                fm<1?0: accu + calculateFuel(fuelForMass)}.memoize()
-            fuel + fuelForFuel(fuelForMass)
-        }
+        masses*.toInteger().inject(0, calculateExtrafuel)
     }
 
+    Closure calculateExtrafuel = {total, mass ->
+        println "mass :" +mass
+        def fuelForMass = calculateFuel(total, mass)
+        println "fuelForMass :" +fuelForMass
+        int extra = calculateFuel(total,fuelForMass)
+        println "extra :" +extra
+        if(extra<3) total else calculateFuel(total, extra)
+    }
+
+    //fib = { long n -> n<2?n:fib(n-1)+fib(n-2) }.memoize()
+
+/*
     def factorial
     factorial = { int n, def accu = 0 ->
         if (n < 1) return accu
         factorial.trampoline(n - 1, n * accu)
     }
-    factorial = factorial.trampoline()
+    factorial = factorial.trampoline()*/
 }
