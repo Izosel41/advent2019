df33302eea41226d2bf3e4bd15775b5a44576021
Izosel41 <31371831+Izosel41@users.noreply.github.com>
2019-12-11 08:13:20 +0100

    day 6

--- src/main/groovy/D06.groovy
+++ src/main/groovy/D06.groovy
@@ -2,61 +2,14 @@ import java.util.stream.Collectors
 
 class D06 {
 
-    Planet createOrbit(String cmd) {
-        def orb = cmd.tokenize(")")
-        Planet a = new Planet(orb[0])
-        Planet b = new Planet(orb[1])
-        b.orbits(a)
-        a
-    }
-
-    Planet createOrbits(List<String> cmds) {
-        List<Planet> planets = new ArrayList<>()
+    String[][] createOrbits(List<String> cmds) {
+        def planets = new ArrayList<>()
 
         for (String c : cmds) {
-            Planet p = createOrbit(c)
-            planets.add(p)
+            def p = c.tokenize("(")
+            planets[p[0]] = p[1]
         }
 
-        List<String> names = Collections.singletonList("COM")
-        Planet root = new Planet("")
 
-        for (String name : names) {
-            List<Planet> pls = planets.stream().filter({ it.parent.name == name }).collect(Collectors.toList())
-            names.remove()
-            for (Planet p : pls) {
-                planets.remove(p)
-                p.orbits(root)
-                names.add(p.name)
-            }
-        }
-    }
-}
-
-public class Planet {
-    String name
-    Planet parent
-
-    Planet(String name) {
-        this.name = name
-    }
-
-    void orbits(Planet p) {
-        this.parent = p
-    }
-
-    boolean equals(o) {
-        if (this.is(o)) return true
-        if (getClass() != o.class) return false
-
-        Planet planet = (Planet) o
-
-        if (name != planet.name) return false
-
-        return true
-    }
-
-    int hashCode() {
-        return (name != null ? name.hashCode() : 0)
     }
 }
--- src/test/groovy/D06Test.groovy
+++ src/test/groovy/D06Test.groovy
@@ -2,17 +2,16 @@ class D06Test extends GroovyTestCase {
 
     D06 day = new D06()
 
-    void testCreateOrbit() {
-        Planet a = day.createOrbit("AAA)BBB")
-        assert a != null
-        assert a.next != null
-        assert a.name == "AAA"
-        assert a.next[0].name == "BBB"
-        assert a.next[0].next == null
-    }
-
     void testOrbits() {
-        day.createOrbits(Util.extractLines("61.txt"))
+        def planets = day.createOrbits(Util.extractLines("61.txt"))
+
+
+/*
+        D directly orbits C and indirectly orbits B and COM, a total of 3 orbits.
+        L directly orbits K and indirectly orbits J, E, D, C, B, and COM, a total of 7 orbits.
+        COM orbits nothing.
+*/
+
     }
 
     void testFirst() {
