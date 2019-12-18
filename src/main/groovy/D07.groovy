9d95253940bcf41c76fed261c55671f28acefe75
Izosel41 <31371831+Izosel41@users.noreply.github.com>
2019-12-18 08:46:29 +0100

    day7.2

--- src/main/groovy/D07.groovy
+++ src/main/groovy/D07.groovy
@@ -17,17 +17,24 @@ class D07 {
 
     public Integer feedbackLoop(def phases, int inputSignal, List<Integer> program) {
         IntCode code = new IntCode()
-        //   println phases
-        for (int i = 0; i < phases.size(); i++) {
-            //    println "phase" + phases[i]
-            //    println "input " + inputSignal
-            code.io.add(phases[i])
-            code.io.add(inputSignal)
-            code.run(program)
-            inputSignal = code.io.removeFirst()
+        def loop = true
+        def thruster = 0
+        while(loop) {
+            for (int i = 0; i < phases.size(); i++) {
+                thruster = inputSignal
+                //    println "phase" + phases[i]
+                //    println "input " + inputSignal
+                code.io.add(phases[i])
+                code.io.add(inputSignal)
+                code.run(program)
+                inputSignal = code.io.removeFirst()
+
+                if (inputSignal==99)
+                    loop = false
+                println inputSignal
+            }
         }
-        //   println inputSignal
-        inputSignal
+        thruster
     }
 
 }
